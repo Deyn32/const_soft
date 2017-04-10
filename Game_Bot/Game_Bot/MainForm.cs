3e16a146cec5682eed31117487888caf7d9324bf
Dmitriy Mikhailov <deyn32@yandex.ru>
2017-04-10 22:26:32 +0300

    Добавление С++ проекта в общий

--- Game_Bot/Game_Bot/MainForm.cs
+++ Game_Bot/Game_Bot/MainForm.cs
@@ -8,14 +8,16 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace Game_Bot
 {
     public partial class MainForm : Form
     {
         public MainForm()
         {
-            //Михайлов 10.04.2017 Буду делать то-то и то-то
+            //Михайлов 10.04.2017 Буду делать межъязыковое обобщение, пока не получилось((
             InitializeComponent();
+            
         }
     }
 }
