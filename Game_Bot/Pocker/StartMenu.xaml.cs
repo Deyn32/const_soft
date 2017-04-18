e256e05767e2c2bd6ccdf5c1afce704fe73a40c8
Dmitriy Mikhailov <deyn32@yandex.ru>
2017-04-18 21:44:08 +0300

    Сделал столик в главном WPF проекте

---
+++ Game_Bot/Pocker/StartMenu.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Pocker
+{
+    /// <summary>
+    /// Логика взаимодействия для StartMenu.xaml
+    /// </summary>
+    public partial class StartMenu: Window
+    {
+        public StartMenu()
+        {
+            //InitializeComponent();
+        }
+    }
+}
